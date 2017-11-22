dfa1f82bf8a5c77e1037d6b18874d6bb74c03299
Sergey Maksimenko <s.maxymenko@gmail.com>
2017-11-23 01:16:29 +0200

    Create basic functional
    Add counting method

--- Mine_acad_project_1/basis_main.py
+++ Mine_acad_project_1/basis_main.py
@@ -31,15 +31,32 @@ class Main(QtGui.QMainWindow):  # QMainWindow is a base class with needed method
         self.today = datetime.date.today()
 
         self.days_counter()
+        self.counting()
 
         # conections
         QObject.connect(self.ui.date_start, QtCore.SIGNAL(_fromUtf8("dateChanged(QDate)")), self.days_counter)
         QObject.connect(self.ui.date_end, QtCore.SIGNAL(_fromUtf8("dateChanged(QDate)")), self.days_counter)
+        QObject.connect(self.ui.spinBox_prepainment, QtCore.SIGNAL(_fromUtf8("valueChanged(QString)")), self.counting)
+        QObject.connect(self.ui.SpinBox_square, QtCore.SIGNAL(_fromUtf8("valueChanged(QString)")), self.counting)
+        QObject.connect(self.ui.spinBox_price, QtCore.SIGNAL(_fromUtf8("valueChanged(QString)")), self.counting)
 
     def days_counter(self):
         first_day = self.ui.date_start.date()
         last_day = self.ui.date_end.date()
-        print(first_day, last_day) #debug
+        different_from_today = self.today - first_day.toPyDate()
+        all_term = last_day.toPyDate() - first_day.toPyDate()
+        Main.month_total = round(all_term.days / 30)
+        Main.month_from_now = round(different_from_today.days / 30)
+        #print( Main.month_total, Main.month_from_now) #debug
+        self.counting()
+    
+    def counting(self):
+        Main.total_price = round(self.ui.SpinBox_square.value() * self.ui.spinBox_price.value())
+        Main.balance_amount = Main.total_price - self.ui.spinBox_prepainment.value()
+        Main.month_payment = Main.balance_amount / (Main.month_total-1)
+        self.ui.spinBox_total.setValue(int(Main.total_price))
+        self.ui.spinBox_balance_amount.setValue(int(Main.balance_amount))
+        self.ui.spinBox_mon_pay.setValue(int(Main.month_payment))
 
 
 
